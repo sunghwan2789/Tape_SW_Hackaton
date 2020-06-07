36d7557fcb2d6ccc198187631a630a2d0495e005
Sunghwan Bang <sunghwan2789@gmail.com>
2020-06-08 02:38:10 +0900

    log messages and print result

--- tape/main.py
+++ tape/main.py
@@ -28,15 +28,29 @@ def main():
     audio_manager = AudioManager(audio)
     transcriber = Transcriber(audio_manager)
 
+    filter_sections = []
+
     for transcribtion in transcriber.transcribe():
-        print(transcribtion)
+        logger.debug("transcription: %s", transcribtion)
         word, start, end = transcribtion
         if detect_mature_word(word):
+            logger.debug("mature word: %s, %s", word, detect_mature_word(word))
             audio_manager.apply_beep(start, end)
             manager.apply_mask(start, end)
+            filter_sections.append({"start_time": start, "end_time": end, "word": word})
 
     manager.apply_audio(audio_manager.save(settings.ROOT / "output/a_beep.wav"))
     manager.save(settings.ROOT / "output/a.mp4")
+
+    print(
+        json.dumps(
+            {
+                "thumbnail": str(thumbnail),
+                "filter_sections": filter_sections,
+                "filter_video": str(settings.ROOT / "output" / "a.mp4"),
+            }
+        )
+    )
 
 
 if __name__ == "__main__":
