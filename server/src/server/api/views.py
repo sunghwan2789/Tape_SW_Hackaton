45cd2077ba32519a1b75fb61b92a1cc1483013f1
Sunghwan Bang <sunghwan2789@gmail.com>
2020-06-08 04:03:02 +0900

    use filter result from tape

--- server/src/server/api/views.py
+++ server/src/server/api/views.py
@@ -1,23 +1,30 @@
-from django.shortcuts import render
-from django.http.response import JsonResponse, HttpResponseBadRequest
-from rest_framework.parsers import FormParser, MultiPartParser
-from rest_framework import serializers, viewsets
-from .models import *
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
-
+import json
 import os
 import shutil
+import subprocess
+import tempfile
+from pathlib import Path
+
+from django.conf import settings
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.core.files.uploadedfile import TemporaryUploadedFile, UploadedFile
+from django.http.response import HttpResponseBadRequest, JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers, viewsets
+from rest_framework.parsers import FormParser, MultiPartParser
+
+from .models import *
+
 # Create your views here.
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -28,7 +35,7 @@ class UserViewSet(viewsets.ModelViewSet):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -39,8 +46,17 @@ class ProfileViewSet(viewsets.ModelViewSet):
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = ['id', 'user', 'title', 'filepath',
-                  'filterpath', 'pub_date', 'thumbnail', 'description']
+        fields = [
+            "id",
+            "user",
+            "title",
+            "filepath",
+            "filterpath",
+            "pub_date",
+            "thumbnail",
+            "description",
+        ]
+
     user = serializers.SerializerMethodField()
 
     def get_user(self, obj):
@@ -48,18 +64,22 @@ class VideoSerializer(serializers.ModelSerializer):
 
 
 class VideoViewSet(viewsets.ModelViewSet):
-    queryset = Video.objects.order_by('-id')
+    queryset = Video.objects.order_by("-id")
     serializer_class = VideoSerializer
-    parser_classes = (MultiPartParser, FormParser,)
-    read_only_fields = ('user',)
+    parser_classes = (
+        MultiPartParser,
+        FormParser,
+    )
+    read_only_fields = ("user",)
 
     def get_queryset(self):
-        queryset = Video.objects.order_by('-id')
+        queryset = Video.objects.order_by("-id")
 
-        q = self.request.query_params.get('q', None)
+        q = self.request.query_params.get("q", None)
         if q is not None:
             queryset = queryset.filter(title__contains=q) | queryset.filter(
-                description__contains=q)
+                description__contains=q
+            )
 
         return queryset
 
@@ -71,33 +91,36 @@ class VideoViewSet(viewsets.ModelViewSet):
         4. filtering된 영상 db에 가져와 저장.
         5. app 화면에는 filtering된 영상 업로드.
         """
-        upload_file = self.request.data.get('filepath')
+        upload_file: UploadedFile = self.request.data.get("filepath")
 
-        tape_input = os.path.join(settings.TAPE_ROOT, 'input/')
-        tape_input_video = os.path.join(tape_input, 'test2.mp4')
-
-        # copy raw video into tape input directory
-        with open(tape_input_video, 'wb') as f:
+        with tempfile.NamedTemporaryFile("wb") as temp_uploaded_video:
+            # copy raw video into tape input directory
             for chunk in upload_file.chunks():
-                f.write(chunk)
+                temp_uploaded_video.file.write(chunk)
 
-        # 상대 경로이기 때문에 cd로 해당 루트로 들어가서 실행.
-        # system 첫 호출시 위치는 root.
-        os.system("cd " + settings.TAPE_ROOT + " && python main.py")
+            # 상대 경로이기 때문에 cd로 해당 루트로 들어가서 실행.
+            # system 첫 호출시 위치는 root.
+            completed_subprocess = subprocess.run(
+                ["python", "main.py", temp_uploaded_video.name],
+                cwd=settings.TAPE_ROOT,
+                stdout=subprocess.PIPE,
+            )
+            completed_subprocess.check_returncode()
 
-        # Get reult file & save the file at filtering field
-        output_path = os.path.join(settings.TAPE_ROOT, 'output/')
+            tape_result = json.loads(completed_subprocess.stdout)
 
         result_filename = "filter_" + upload_file.name
-        thumnail_filename = "filter_" + upload_file.name + '.jpg'
+        thumnail_filename = "filter_" + upload_file.name + ".jpg"
 
         # copy media directory
         fs = FileSystemStorage()
-        result_filename = fs.save(result_filename, open(
-            output_path + "test2.mp4", "rb"))
+        result_filename = fs.save(
+            result_filename, open(tape_result['filter_video'], "rb")
+        )
         fs = FileSystemStorage()
         thumnail_filename = fs.save(
-            thumnail_filename, open(output_path + "test2.jpg", 'rb'))
+            thumnail_filename, open(tape_result['thumbnail'], "rb")
+        )
         fs = FileSystemStorage()
         upload_filename = fs.save(None, upload_file)
 
@@ -106,10 +129,10 @@ class VideoViewSet(viewsets.ModelViewSet):
         os.system('chmod +r -R "' + settings.MEDIA_ROOT + '"')
 
         serializer.save(
-            user=User.objects.get(pk=self.request.data.get('user', 1)),
-            title=self.request.data.get('title'),
+            user=User.objects.get(pk=self.request.data.get("user", 1)),
+            title=self.request.data.get("title"),
             filepath=upload_filename,
-            description=self.request.data.get('description'),
+            description=self.request.data.get("description"),
             filterpath=result_filename,
             thumbnail=thumnail_filename,
         )
@@ -119,32 +142,26 @@ class VideoViewSet(viewsets.ModelViewSet):
 def login(request):
     try:
         user = auth.authenticate(
-            request, username=request.POST['phone'], password=request.POST['password'])
+            request, username=request.POST["phone"], password=request.POST["password"]
+        )
         if user is not None:
             auth.login(request, user)
-            return JsonResponse({
-                'id': user.id,
-            })
-        raise ValueError('user not found or incorrect password!')
+            return JsonResponse({"id": user.id,})
+        raise ValueError("user not found or incorrect password!")
     except Exception as ex:
-        return JsonResponse({
-            'error': str(ex),
-        })
+        return JsonResponse({"error": str(ex),})
 
 
 @csrf_exempt
 def signup(request):
     try:
         user = User.objects.create_user(
-            username=request.POST['phone'], password=request.POST['password'])
-        user.profile.name = request.POST['name']
-        user.profile.passwordQuestion = request.POST['password_question']
-        user.profile.passwordQuestionAnswer = request.POST['password_question_answer']
+            username=request.POST["phone"], password=request.POST["password"]
+        )
+        user.profile.name = request.POST["name"]
+        user.profile.passwordQuestion = request.POST["password_question"]
+        user.profile.passwordQuestionAnswer = request.POST["password_question_answer"]
         auth.login(request, user)
-        return JsonResponse({
-            'id': user.id,
-        })
+        return JsonResponse({"id": user.id,})
     except Exception as ex:
-        return JsonResponse({
-            'error': str(ex),
-        })
+        return JsonResponse({"error": str(ex),})
